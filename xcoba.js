85708d2453a6f3611b04c5eb1598608620c3f82d
bipproduction <bip.production.js@gmail.com>
2023-09-28 14:48:05 +0800

    title: auto des: auto note:auto

--- xcoba.js
+++ xcoba.js
@@ -1,5 +1,7 @@
-const yaml = require('yaml')
-const fs = require('fs')
+require('dotenv').config()
+console.log(process.env.MY_PORT)
+// const yaml = require('yaml')
+// const fs = require('fs')
 
-const data  = fs.readFileSync('./list_server.yaml').toString()
-console.log(yaml.parse(data))
+// const data  = fs.readFileSync('./list_server.yaml').toString()
+// console.log(yaml.parse(data))
